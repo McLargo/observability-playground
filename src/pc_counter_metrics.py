dcacc73da18045360a2503f209fa3c3e1e81cd69
Javier Gil <javiergilmont@gmail.com>
2025-07-23 12:07:45 +0200

    Update alerts

--- src/pc_counter_metrics.py
+++ src/pc_counter_metrics.py
@@ -10,6 +10,16 @@ demo_count = Counter(
     ["event_type", "status"]
 )
 
+def init_metrics():
+    demo_count.labels(event_type=EventType.LOGIN, status=EventStatus.SUCCESS).inc(0)
+    demo_count.labels(event_type=EventType.LOGIN, status=EventStatus.FAILURE).inc(0)
+    demo_count.labels(event_type=EventType.DATA_PROCESSING, status=EventStatus.SUCCESS).inc(0)
+    demo_count.labels(event_type=EventType.DATA_PROCESSING, status=EventStatus.FAILURE).inc(0)
+    # wait until the metrics are initialized
+    print("Metrics initialized, waiting for 15 seconds before processing events...")
+    time.sleep(15)
+
+
 
 def process_event(event_type, status):
     """
@@ -24,6 +34,8 @@ if __name__ == "__main__":
     # to allow scraping of metrics.
     start_http_server(8880)
     print("Prometheus metrics server started on port 8880")
+
+    init_metrics()
 
     # simulate some events
     process_event(EventType.LOGIN, EventStatus.SUCCESS)
--- src/pg_counter_metrics.py
+++ src/pg_counter_metrics.py
@@ -1,3 +1,4 @@
+import time
 import uuid
 
 from prometheus_client import CollectorRegistry, Counter, push_to_gateway
@@ -14,16 +15,28 @@ demo_count = Counter(
     registry=registry,
 )
 
+def init_metrics():
+    demo_count.labels(event_type=EventType.LOGIN, status=EventStatus.SUCCESS).inc(0)
+    demo_count.labels(event_type=EventType.LOGIN, status=EventStatus.FAILURE).inc(0)
+    demo_count.labels(event_type=EventType.DATA_PROCESSING, status=EventStatus.SUCCESS).inc(0)
+    demo_count.labels(event_type=EventType.DATA_PROCESSING, status=EventStatus.FAILURE).inc(0)
+    # wait until the metrics are initialized
+    print("Metrics initialized, waiting for 15 seconds before processing events...")
+    time.sleep(15)
+
+
 
 def process_event(event_type, status):
     """
-    Simulates processing an event in the push_gatewaylication.
+    Simulates processing an event in the push_gateway application.
     """
     print(f"Event simulation | demo_push_gateway_process_events_total | {event_type} | {status}")
     demo_count.labels(event_type=event_type, status=status).inc()
 
 
 if __name__ == "__main__":
+    init_metrics()
+
     # simulate some events
     process_event(EventType.LOGIN, EventStatus.SUCCESS)
     process_event(EventType.LOGIN, EventStatus.FAILURE)
